44c5f76f7c46195d8eaeca9ea08386c5343342c0
priyansu mohanty <mohantypriyansu02@gmail.com>
2025-12-05 21:32:28 +0530

    Time: 12 ms (50.75%) | Memory: 56.5 MB (28.90%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int change(int sum, int[] coin) {
+        int n= coin.length;
+        int dp[][]=new int[n+1][sum+1];
+
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=0;i<sum+1;i++){
+            dp[0][i]=0;
+        }
+
+        for(int i=1; i< n+1; i++){
+            for(int j=1; j < sum+1; j++){
+                if(coin[i-1] <= j){
+                    dp[i][j]=dp[i][j-coin[i-1]] + dp[i-1][j];
+                }else{
+                    dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
