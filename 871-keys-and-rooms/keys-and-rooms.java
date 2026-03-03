d2fa43a58691e50216e18a31a96db01c2f844519
priyansu mohanty <mohantypriyansu02@gmail.com>
2026-03-03 07:15:44 +0530

    Time: 1 ms (85.94%) | Memory: 46.3 MB (82.71%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean visited[]=new boolean[rooms.size()];
+        dfs(rooms.get(0),0,rooms,visited);
+
+        for(int i=0;i<visited.length;i++){
+            if(!visited[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void dfs(List<Integer>keys, int room, List<List<Integer>>rooms,boolean visited[]){
+        visited[room]=true;
+
+        for(Integer i:keys){
+            if(!visited[i]){
+                dfs(rooms.get(i),i,rooms,visited);
+            }
+        }
+    }
+
+}
