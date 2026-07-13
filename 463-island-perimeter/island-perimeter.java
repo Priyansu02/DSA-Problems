45c573a79eaf24725ff8b0fa2595e3fa25055fe8
priyansu mohanty <mohantypriyansu02@gmail.com>
2026-07-13 18:33:14 +0530

    Time: 4 ms (99.90%) | Memory: 47.6 MB (12.17%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int land=0, nei=0;
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j] == 1){
+                    land++;
+
+                    if(i< grid.length-1 && grid[i+1][j]== 1)nei++;
+                    if(j< grid[i].length-1 && grid[i][j+1]== 1)nei++;
+
+                }
+            }
+        }
+
+        return land*4 - nei*2;
+    }
+}
