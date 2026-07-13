38aaa6ddc1a6391a4f11a85affe8f09fcbe91a72
priyansu mohanty <mohantypriyansu02@gmail.com>
2026-07-13 18:46:44 +0530

    Time: 8 ms (17.25%) | Memory: 47.3 MB (38.36%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,19 +1,29 @@
 class Solution {
+    int perimeter = 0;
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    
     public int islandPerimeter(int[][] grid) {
-        int land=0, nei=0;
-
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length;j++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == 1){
-                    land++;
-
-                    if(i< grid.length-1 && grid[i+1][j]== 1)nei++;
-                    if(j< grid[i].length-1 && grid[i][j+1]== 1)nei++;
-
+                    dfs(grid, i, j);
+                    return perimeter; // only one island
                 }
             }
         }
+        return 0;
+    }
 
-        return land*4 - nei*2;
+    void dfs(int[][] grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0){
+            perimeter++; // water or boundary contributes to perimeter
+            return;
+        }
+        if(grid[i][j] == -1) return; // already visited
+        
+        grid[i][j] = -1; // mark visited
+        for(int[] d : dirs){
+            dfs(grid, i+d[0], j+d[1]);
+        }
     }
 }
